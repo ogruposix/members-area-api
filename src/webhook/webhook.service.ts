977407a4017f8699eb85b8f617825ca694721114
mtguerson <mtguerson@gmail.com>
2025-04-25 17:25:33 -0400

    feat(test): test cart panda webhook

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -28,49 +28,8 @@ export class WebhookService {
     this.token = token;
   }
 
-  async getOrders(): Promise<WebhookResponse> {
-    try {
-      const response = await axios.get(this.apiUrl, {
-        headers: {
-          Authorization: `Bearer ${this.token}`,
-        },
-      });
-
-      const orders = response.data.orders.data;
-
-      const result = orders.map((order: any) => ({
-        email: order.email,
-        title: order.line_items.map((item: any) => item.title),
-      }));
-
-      return result;
-    } catch (error) {
-      console.error("Erro ao fazer a requisição à API:", error);
-      throw new Error("Erro ao processar a requisição");
-    }
-  }
-
-  @Cron("*/5 * * * *")
-  async handleCron() {
-    console.log("Executando cron job...");
-
-    const orders = await this.getOrders();
-
-    if (!Array.isArray(orders)) {
-      console.error("Os pedidos retornados não são um array");
-      return;
-    }
-
-    for (const order of orders) {
-      const { email, title } = order;
-
-      const existingUser = await this.userService.getUserByEmail(email);
-
-      if (existingUser) {
-        return null;
-      }
-      await this.userService.createUser(email, title);
-      console.log(`Usuário criado: ${email}`);
-    }
+  async getOrders(): Promise<any> {
+    console.log("Hello World");
+    return "Hello World";
   }
 }
