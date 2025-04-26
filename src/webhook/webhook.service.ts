14a4d819bc42c41fc911775fb45fd239d24fe468
mtguerson <mtguerson@gmail.com>
2025-04-26 13:25:36 -0400

    fix: change the payload order console log

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -37,7 +37,7 @@ export class WebhookService {
     console.log(
       "Payload recebido:",
       payload.order.email,
-      payload.order.line_items.title
+      payload.order.line_items.title.map((item: any) => item.title)
     );
 
     await this.userService.createUser(
