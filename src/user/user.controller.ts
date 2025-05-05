fe9573f8eec20e535fb044ff86cc5dcf259ae01c
mtguerson <mtguerson@gmail.com>
2025-05-05 13:40:13 -0400

    feat: add change the get user by email route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,21 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Controller, Get, Param, Query } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 @Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  async getUserByEmail(@Query("email") email: string) {
-    const user = await this.userService.getUserByEmail(email);
-
-    return user;
-  }
-
   @Get("products")
   async getUserProducts(@Query("email") email: string) {
     const products = await this.userService.getProductsByEmail(email);
 
     return { products };
+  }
+
+  @Get(":email")
+  async getUserByEmail(@Param("email") email: string) {
+    const user = await this.userService.getUserByEmail(email);
+
+    return user;
   }
 }
