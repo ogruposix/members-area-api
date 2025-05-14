7d36c85d61df528e559bc85ad2b582b490900ea1
mtguerson <mtguerson@gmail.com>
2025-05-14 13:13:33 -0400

    feat(test): payload test for cartpanda webhook

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { Public } from "./decorators/public.decorator";
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Public()
+  @Post()
+  async testPayload(@Body() payload: any) {
+    console.log(payload || "No payload");
+  }
+}
